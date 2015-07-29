01f01ce1c865e066966ca7505de2a2c5257e693c
Nigel Cunningham <nigelc@catalyst-au.net>
2015-07-29 15:00:13 +1000

    Add initial version of webservice code for getting status via ajax.
    Signed-off-by: Nigel Cunningham <nigelc@catalyst-au.net>

---
+++ client/client.php
@@ -0,0 +1,41 @@
+<?php
+// This client for local_wstemplate is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+
+/**
+ * XMLRPC client for Moodle 2 - local_wstemplate
+ *
+ * This script does not depend of any Moodle code,
+ * and it can be called from a browser.
+ *
+ * @authorr Jerome Mouneyrac
+ */
+
+/// MOODLE ADMINISTRATION SETUP STEPS
+// 1- Install the plugin
+// 2- Enable web service advance feature (Admin > Advanced features)
+// 3- Enable XMLRPC protocol (Admin > Plugins > Web services > Manage protocols)
+// 4- Create a token for a specific user and for the service 'My service' (Admin > Plugins > Web services > Manage tokens)
+// 5- Run this script directly from your browser: you should see 'Hello, FIRSTNAME'
+
+/// SETUP - NEED TO BE CHANGED
+$token = 'dff3bc6a9368d3b0db1ef59f6760ef7c';
+$domainname = 'http://YOURMOODLE';
+
+/// FUNCTION NAME
+$functionname = 'local_wstemplate_hello_world';
+
+/// PARAMETERS
+$welcomemsg = 'Hello, ';
+
+///// XML-RPC CALL
+header('Content-Type: text/plain');
+$serverurl = $domainname . '/webservice/xmlrpc/server.php'. '?wstoken=' . $token;
+require_once('./curl.php');
+$curl = new curl;
+$post = xmlrpc_encode_request($functionname, array($welcomemsg));
+$resp = xmlrpc_decode($curl->post($serverurl, $post));
+print_r($resp);
