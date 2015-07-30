57bac7182af0a8f8eba08edc9489fdebaf77d5a3
Nigel Cunningham <nigelc@catalyst-au.net>
2015-07-31 09:46:28 +1000

    Import Dim's user cleaning function and modify to do in bulk.
    Signed-off-by: Nigel Cunningham <nigelc@catalyst-au.net>

--- cleaner/users/classes/clean.php
+++ cleaner/users/classes/clean.php
@@ -25,7 +25,75 @@ namespace cleaner_users;
 defined('MOODLE_INTERNAL') || die();
 
 class clean extends \local_datacleaner\clean {
+    /**
+     * Return the ID for an item.
+     *
+     * @param  object item The item from which to return the ID.
+     * @return int    id   The item id.
+     */
+    protected static function get_item_id($item) {
+        return $item->id;
+    }
 
+    /**
+     * Delete a group of users
+     *
+     * Based on the Ducere migration code originally written by Dima.
+     *
+     * @param array $users User IDs to delete.
+     */
+    private function delete_users(array $users) {
+        global $DB;
+
+        if (empty($users)) {
+            return;
+        }
+
+        foreach ($users as $user) {
+            delete_user($user);
+        }
+
+        // Clean up Assignment stuff.
+        $userids = array_map(self::get_item_id, $users);
+        list($userinequal, $userparams) = $DB->get_in_or_equal($userids);
+
+        $submissions = $DB->get_fieldset_select("assign_submission", "id", $userinequal, $userparams);
+        if (!empty($submissions)) {
+            // TODO: Actually delete the files.
+            $DB->delete_records_list('assignsubmission_file', 'submission', $submissions);
+            $DB->delete_records_list('assignsubmission_onlinetext', 'submission', $submissions);
+        }
+
+        $DB->delete_records_list('assign_submission', 'userid', $userids);
+
+        $grades = $DB->get_fieldset_select("assign_grades", "userid", $userinequal, $userparams);
+        if (!empty($grades)) {
+            $DB->delete_records_list('assignfeedback_comments', 'grade', $grades);
+            // TODO: Actually delete the files.
+            $DB->delete_records_list('assignfeedback_file', 'grade', $grades);
+            $DB->delete_records_list('assignfeedback_editpdf_annot', 'gradeid', $grades);
+            $DB->delete_records_list('assignfeedback_editpdf_cmnt', 'gradeid', $grades);
+        }
+        $DB->delete_records_list('assign_grades', 'userid', $userids);
+        $DB->delete_records_list('assign_user_flags', 'userid', $userids);
+        $DB->delete_records_list('assign_user_mapping', 'userid', $userids);
+        $DB->delete_records_list('assignfeedback_editpdf_quick', 'userid', $userids);
+        // Clean up local messages.
+        $DB->delete_records_list('local_messages_sent', 'userid', $userids);
+        // Clean up leaderboard.
+        $DB->delete_records_list('block_leaderboard_data', 'userid', $userids);
+        $DB->delete_records_list('block_leaderboard_points', 'userid', $userids);
+        // Finally clean up user table.
+        $DB->delete_records_list('user', 'id', $userids);
+
+        foreach ($users as $user) {
+            mtrace(" Deleted user for ".fullname($user, true)." ($user->id)");
+        }
+    }
+
+    /**
+     * Do the hard work of cleaning up users.
+     */
     static public function execute() {
 
         global $DB;
