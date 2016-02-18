b4d02ed107c8935c43ae72debd9de5aef93db243
Nigel Cunningham <nigelc@catalyst-au.net>
2016-02-18 11:13:34 +1100

    WR246226: Add simple CLI script to get sitefile list.
    (To be run post-datawash so we can then sync from prod only
     the files that are needed).
    
    Signed-off-by: Nigel Cunningham <nigelc@catalyst-au.net>

---
+++ cli/sitedata_file_list.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    local_datacleaner
+ * @copyright  2015 Nigel Cunningham <nigelc@catalyst-au.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+require(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+require_once($CFG->libdir.'/clilib.php');
+
+global $DB;
+
+$sql = 'SELECT DISTINCT contenthash FROM {files} WHERE filename != "."';
+$result = $DB->get_records_sql($sql);
+foreach($result as $hash) {
+    echo "{$hash->contenthash}\n";
+}
