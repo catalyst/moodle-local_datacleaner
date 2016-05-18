8f46e35638c8b1d53a536060fa14a3f00f7d0eca
Marcus Boon <(none)>
2016-05-18 12:47:11 +1000

    Bumped version number to include all the fixes

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016032300;
-$plugin->release   = '2.3.3';
+$plugin->version   = 2016051800;
+$plugin->release   = '2.3.4';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2013111800; // Moodle 2.6 release and upwards.
 $plugin->component = 'local_datacleaner';
