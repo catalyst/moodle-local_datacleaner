50548ed0e0f3eb96946c4bf0c4d766a0fe7f2d95
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-04-11 09:44:08 +1000

    Update the cleaner core manage url.
    The admin page settings.php?section=local_cleaner is no longer used for managing the plugins.

--- classes/plugininfo/cleaner.php
+++ classes/plugininfo/cleaner.php
@@ -89,7 +89,7 @@ class cleaner extends base {
      * @return \moodle_url
      */
     public static function get_manage_url() {
-        return new \moodle_url('/admin/settings.php', array('section' => 'local_cleaner'));
+        return new \moodle_url('/local/datacleaner/index.php');
     }
 
     /**
