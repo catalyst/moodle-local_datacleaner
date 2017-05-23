2b6b9108e733ca6fd0b3dd55ffb573314e1bf7fa
Nicholas Hoobin <nicholashoobin@catalyst-au.net>
2017-05-23 10:46:36 +1000

    Update JS keypress to operate on textareas.

--- cleaner/environment_matrix/amd/src/matrix.js
+++ cleaner/environment_matrix/amd/src/matrix.js
@@ -1,9 +1,12 @@
 define(['jquery'], function($) {
     return {
         init: function() {
-            $(":text").keypress(function() {
-                var cb = $(this).closest('.fgroup').find('input[type="checkbox"]');
-                cb.attr('checked', true);
+            $("input[type=text], textarea").each(function() {
+                var input = $(this);
+                input.keypress(function() {
+                    var cb = $(this).closest('.fgroup').find('input[type="checkbox"]');
+                    cb.attr('checked', true);
+                });
             });
         }
     };
