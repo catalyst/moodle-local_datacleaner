2249608c1607d67d9f00fc6e9c0c524842af7aa8
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-07-04 11:58:12 +1000

    Added uploader.

---
+++ cleaner/muc/tests/phpunit/output/uploader_test.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     cleaner_muc
+ * @subpackage  local_cleanurls
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use cleaner_muc\output\uploader;
+
+defined('MOODLE_INTERNAL') || die();
+
+class  local_cleanurls_cleaner_muc_output_uploader_test extends advanced_testcase {
+    const DOWNLOAD_LINK = '/local/datacleaner/cleaner/muc/downloader.php?sesskey=';
+
+    public static function setUpBeforeClass() {
+        parent::setUpBeforeClass();
+
+        // Trigger classloaders.
+        class_exists(uploader::class);
+    }
+
+    protected function setUp() {
+        parent::setUp();
+        $this->resetAfterTest(true);
+        self::setAdminUser();
+    }
+
+    public function test_it_exists() {
+        self::assertInstanceOf(uploader::class, new uploader());
+    }
+}
