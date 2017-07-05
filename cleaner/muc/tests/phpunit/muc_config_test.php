97b06c587ddae1a04a52e17ee29b71d33ea159f3
Daniel Thee Roperto <danielroperto@catalyst-au.net>
2017-07-05 17:29:16 +1000

    MUC Cleaner - Created muc_config class as it will hold more information than just config.

---
+++ cleaner/muc/tests/phpunit/muc_config_test.php
@@ -0,0 +1,116 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     cleaner_muc
+ * @subpackage  local_cleanurls
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use cleaner_muc\muc_config;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests.
+ *
+ * @package     cleaner_muc
+ * @subpackage  local_cleanurls
+ * @author      Daniel Thee Roperto <daniel.roperto@catalyst-au.net>
+ * @copyright   2017 Catalyst IT Australia {@link http://www.catalyst-au.net}
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @SuppressWarnings(public) Allow as many methods as needed.
+ */
+class local_cleanurls_cleaner_muc_config_test extends advanced_testcase {
+    public function test_it_has_the_required_fields() {
+        $expected = ['id', 'wwwroot', 'timestamp', 'configuration', 'lastmodified'];
+        $reflection = new ReflectionClass(muc_config::class);
+        $fields = $reflection->getProperties();
+        $actual = [];
+        foreach ($fields as $field) {
+            $actual[] = $field->getName();
+        }
+        self::assertSame($expected, $actual);
+    }
+
+    public function test_it_can_be_created_from_array_and_object() {
+        $inputdata = [
+            'id'            => 1,
+            'wwwroot'       => 'http://moodle.test',
+            'timestamp'     => 3,
+            'configuration' => '<?php // Config',
+            'lastmodified'  => 5,
+        ];
+
+        foreach (['array', 'object'] as $type) {
+            $data = ($type == 'object') ? (object)$inputdata : $inputdata;
+            $config = new muc_config($data);
+
+            $type = "For type {$type}";
+            self::assertSame(1, $config->get_id(), $type);
+            self::assertSame('http://moodle.test', $config->get_wwwroot(), $type);
+            self::assertSame(3, $config->get_timestamp(), $type);
+            self::assertSame('<?php // Config', $config->get_configuration(), $type);
+            self::assertSame(5, $config->get_lastmodified(), $type);
+        }
+    }
+
+    public function test_it_can_be_created_from_another_config() {
+        $data = [
+            'id'            => 1,
+            'wwwroot'       => 'http://moodle.test',
+            'timestamp'     => 3,
+            'configuration' => '<?php // Config',
+            'lastmodified'  => 5,
+        ];
+
+        $config = new muc_config($data);
+        $newconfig = new muc_config($config);
+        self::assertSame(1, $newconfig->get_id());
+        self::assertSame('http://moodle.test', $newconfig->get_wwwroot());
+        self::assertSame(3, $newconfig->get_timestamp());
+        self::assertSame('<?php // Config', $newconfig->get_configuration());
+        self::assertSame(5, $newconfig->get_lastmodified());
+    }
+
+    public function test_it_can_be_converted_back_from_array() {
+        $expected = [
+            'id'            => 1,
+            'wwwroot'       => 'http://moodle.test',
+            'timestamp'     => 3,
+            'configuration' => '<?php // Config',
+            'lastmodified'  => 5,
+        ];
+        $config = new muc_config($expected);
+        $actual = $config->to_array();
+        self::assertSame($expected, $actual);
+    }
+
+    public function test_it_can_have_some_fields_null() {
+        $expected = [
+            'id'            => null,
+            'wwwroot'       => '',
+            'timestamp'     => null,
+            'configuration' => '',
+            'lastmodified'  => null,
+        ];
+        $config = new muc_config([]);
+        $actual = $config->to_array();
+        self::assertSame($expected, $actual);
+    }
+}
