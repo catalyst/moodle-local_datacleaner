fbac67a13a0884ee8bf9db67746d2b8d2d504049
Ilya Tregubov <ilyatregubov@catalyst-au.net>
2018-06-13 14:59:26 +1000

    WR #293347: fix for older moodle versions

--- cleaner/muc/classes/privacy/provider.php
+++ cleaner/muc/classes/privacy/provider.php
@@ -34,13 +34,15 @@ defined('MOODLE_INTERNAL') || die();
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
+    use \core_privacy\local\legacy_polyfill;
+
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function _get_reason() : string {
         return 'privacy:metadata';
     }
 }
