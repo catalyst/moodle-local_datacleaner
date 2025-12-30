a364a7ad866013cd3cb74e0dfd748fb318b587d9
Brendan Heywood <brendan@catalyst-au.net>
2025-12-30 12:13:23 +1100

    Sorted lang file

--- cleaner/orphaned_sitedata/lang/en/cleaner_orphaned_sitedata.php
+++ cleaner/orphaned_sitedata/lang/en/cleaner_orphaned_sitedata.php
@@ -23,32 +23,22 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Cleanup orphaned sitedata';
-$string['privacy:metadata'] = 'The cleaner orphaned sitedata plugin does not store any personal data.';
-
-// Settings page.
 $string['deletebackups'] = 'Delete backups';
 $string['deletebackupsdesc'] = 'Delete all backups (orphaned or not) from site data and the files table.';
-
-$string['deleteorphanedfiles'] = 'Delete orphaned files';
-$string['deleteorphanedfilesdesc'] = 'Delete all orphaned files in site data that are no longer referenced in the database.';
-
-$string['deletetmpfiles'] = 'Delete temporary files';
-$string['deletetmpfilesdesc'] = 'Delete all temporary files.';
-
 $string['deletecachedfiles'] = 'Delete cached files';
 $string['deletecachedfilesdesc'] = 'Delete all cached files.';
-
-$string['woulddeletebackups'] = 'Would delete {$a} backup file(s).';
-$string['willdeletebackups'] = 'Will delete {$a} backup file(s).';
-
-$string['wouldpurgecache'] = 'Would purge the cache.';
-$string['willpurgecache'] = 'Will purge the cache.';
-
-$string['woulddeletetemp'] = 'Would delete temporary files from: {$a}';
-$string['willdeletetemp'] = 'Will delete temporary files: {$a}';
-
-$string['woulddeleteorphanedfiles'] = 'Would delete orphaned file(s).';
-$string['willdeleteorphanedfiles'] = 'Will delete orphaned file(s).';
-
+$string['deleteorphanedfiles'] = 'Delete orphaned files';
+$string['deleteorphanedfilesdesc'] = 'Delete all orphaned files in site data that are no longer referenced in the database.';
+$string['deletetmpfiles'] = 'Delete temporary files';
+$string['deletetmpfilesdesc'] = 'Delete all temporary files.';
 $string['errordeletingfile'] = "ERROR\n\n*** Cound not delete: {\$a}\n\n";
+$string['pluginname'] = 'Cleanup orphaned sitedata';
+$string['privacy:metadata'] = 'The cleaner orphaned sitedata plugin does not store any personal data.';
+$string['willdeletebackups'] = 'Will delete {$a} backup file(s).';
+$string['willdeleteorphanedfiles'] = 'Will delete orphaned file(s).';
+$string['willdeletetemp'] = 'Will delete temporary files: {$a}';
+$string['willpurgecache'] = 'Will purge the cache.';
+$string['woulddeletebackups'] = 'Would delete {$a} backup file(s).';
+$string['woulddeleteorphanedfiles'] = 'Would delete orphaned file(s).';
+$string['woulddeletetemp'] = 'Would delete temporary files from: {$a}';
+$string['wouldpurgecache'] = 'Would purge the cache.';
