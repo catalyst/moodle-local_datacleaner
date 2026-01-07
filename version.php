526d5269d49f959a2d5f620c80246203fa377df6
Dean Chimezie <dcmexy@gmail.com>
2026-01-07 11:11:02 -0500

    Fix unit test issues occuring in Moodle 501 (fixes #207)

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->version   = 2022020303;
 $plugin->release   = 2022020303;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2021051700; // Moodle 3.11 release and upwards.
-$plugin->supports  = [311, 405];
+$plugin->supports  = [501, 501];
 $plugin->component = 'local_datacleaner';
