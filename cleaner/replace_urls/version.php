c19b2983ad162fbdf804ca9fcfd6405c973033f1
Marcus Boon <marcusboon@users.noreply.github.com>
2026-03-18 12:37:54 +1000

    Update cleaner_replace_urls version to a micro bump

--- cleaner/replace_urls/version.php
+++ cleaner/replace_urls/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2026030800;
-$plugin->release   = '2026030800';
+$plugin->version   = 2015072201;
+$plugin->release   = '2.3.3';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2011120500; // Moodle 2.2 release and upwards.
 $plugin->component = 'cleaner_replace_urls';
