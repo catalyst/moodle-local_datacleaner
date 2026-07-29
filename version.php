c32d56b18e7e6a2955e7abbcfdb48dcc3f681627
Brendan Heywood <brendan@catalyst-au.net>
2026-07-30 00:38:45 +1000

    Fixed supported syntax

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->version   = 2022020305;
 $plugin->release   = 2022020305;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2021051700; // Moodle 3.11 release and upwards.
-$plugin->supports  = [311, 405];
+$plugin->supported = [311, 405];
 $plugin->component = 'local_datacleaner';
